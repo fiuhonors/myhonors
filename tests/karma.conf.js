b753b173c407482d4ce6135770575899890af141
Alastair Paragas <alastairparagas@gmail.com>
2015-05-12 13:29:01 -0400

    Firebase-util, UI-router and MockFirebase
    Added Firebase-util which allows us to fuse multiple Firebase paths, UI-router which is more powerful than Angular's own router module and MockFirebase, which allows us to mock Firebase calls out.

--- tests/karma.conf.js
+++ tests/karma.conf.js
@@ -9,8 +9,13 @@ module.exports = function (config) {
 		singleRun: false,
 		files: [
 			'bower_components/angular/angular.js',
+			'bower_components/angular-ui-router/release/angular-ui-router.min.js',
+			'bower_components/firebase/firebase.js',
+			'bower_components/mockfirebase/browser/mockfirebase.js',
 			'bower_components/angular-mocks/angular-mocks.js',
-			'app/**/*.js',
+			'app/**/config.js',
+			'app/**/module.js',
+			'app/**/!(module,config).js',
 			'tests/unit/**/*.spec.js'
 		],
 		reporters: ['progress']

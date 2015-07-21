d41ff04a6d915a6ea4558478e81e0d7bd7a9b418
Alastair Paragas <alastairparagas@gmail.com>
2015-07-20 22:13:12 -0400

    Config File

---
+++ app/.config.js
@@ -0,0 +1,18 @@
+(function (window) {
+	'use strict';
+	
+	window.SETTINGS = {
+		STORAGE: {
+			FIREBASE: {
+				BASE_URL: ""
+			}
+		},
+		COMPILE_PROVIDER: {
+			DEBUG_INFO_ENABLED: true,
+		},
+		HTTP_PROVIDER: {
+			USE_APPLY_ASYNC: true	
+		}
+	};
+	
+}(window));
